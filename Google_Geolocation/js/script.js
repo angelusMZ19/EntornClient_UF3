34eb4309816a311c8b22b95967bb6e0cbf5274aa
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-03-11 19:38:42 +0100

    Concluido Google_Geolocalizacion

--- Google_Geolocation/js/script.js
+++ Google_Geolocation/js/script.js
@@ -1,40 +1,17 @@
-// let geocoder = new google.maps.Geocoder();
-// let findLoc = document.getElementById("findLoc");
-// let ubicacion = document.getElementById("ubicacion");
-// let myLatLng = new google.maps.LatLng(41.390205, 2.154007);
-// let map;
-
-// //let geocoder = new google.maps.Geocoder();
-// let address = "Carrer de la Selva de Mar 211 08020 Barcelona";
-// geocoder.geocode({ 'address': address }, function (results, status) {
-//     if (status == google.maps.GeocoderStatus.OK) {
-//         latitude = results[0].geometry.location.lat();
-//         longitude = results[0].geometry.location.lng();
-//     }
-// });
-
-
-// async function initMap() {
-//   const { Map } = await google.maps.importLibrary("maps");
-
-//   map = new Map(document.getElementById("map"), {
-//     center: { lat: 41.390205, lng: 2.154007 },
-//     zoom: 8,
-//   });
-// }
-
- initMap();
-
 
 let map;
+
 let geocoder = new google.maps.Geocoder();
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+async function initMap() {
+    const {Map} = await google.maps.importLibrary("maps");
+    map = new Map(document.getElementById('map'), {
         center: {lat: 41.390205, lng: 2.154007},
         zoom: 12
     });
 }
+
+initMap();
 
 function findLoc() {
     let address = document.getElementById('adreca').value;
@@ -54,11 +31,11 @@ function findLoc() {
             let marker = new google.maps.Marker({
                 position: {lat: latitude, lng: longitude},
                 map: map,
-                icon: 'URL_DE_LA_IMATGE'
+                icon:  './img/pin.png'
             });
 
             let infowindow = new google.maps.InfoWindow({
-                content: "STRING + html"
+                content:  document.getElementById("adreca").value
             });
 
             marker.addListener('click', function() {
@@ -72,7 +49,7 @@ function findLoc() {
 
 function ActualLoc() {
     if (navigator.geolocation) {
-        navigator.geolocation.ActualLoc(function(position) {
+        navigator.geolocation.getCurrentPosition(function(position) {
             let pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
@@ -84,7 +61,7 @@ function ActualLoc() {
             let marker = new google.maps.Marker({
                 position: pos,
                 map: map,
-                icon: 'URL_DE_LA_IMATGE'
+                icon: './img/pin.png'
             });
 
             let infowindow = new google.maps.InfoWindow({
